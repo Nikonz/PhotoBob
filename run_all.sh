65bab1afb5735a56d645b734d2f43fb2274aeb4c
Nikita <niklarichev@gmail.com>
2016-10-20 00:37:31 +0300

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,7 +0,0 @@
-make
-./build/bin/align ../pictures/60plate.bmp ../0.bmp "$@"
-./build/bin/align ../pictures/00908u.bmp ../1.bmp "$@"
-./build/bin/align ../pictures/00926u.bmp ../2.bmp "$@"
-./build/bin/align ../pictures/00932u.bmp ../3.bmp "$@"
-./build/bin/align ../pictures/00936u.bmp ../4.bmp "$@"
-./build/bin/align ../pictures/00976u.bmp ../5.bmp "$@"
